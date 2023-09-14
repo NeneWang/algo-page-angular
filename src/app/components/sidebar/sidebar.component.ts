9966aa26788fdfbb7910c268064487dfab245c01
NelsonWang <wangnelson2@gmail.com>
2023-09-13 23:14:09 -0300

    sidebar :woman_pilot:

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  header = 'Documentacion de Algoritmos';
+  links: Array<{ text: string, path: string }> = [
+    { text: 'Bubble Sort', path: '#' },
+    { text: 'Merge Sort', path: '#' }
+  ];
 }

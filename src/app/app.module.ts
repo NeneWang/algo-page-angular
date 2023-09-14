46af8edba25319a300d53ecee6f5c5465a230135
NelsonWang <wangnelson2@gmail.com>
2023-09-13 22:43:57 -0300

    componetns tool added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageWrapperComponent } from './components/page-wrapper/page-wrapper.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    SidebarComponent,
+    ToolbarComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule,
